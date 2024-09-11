4c9147c0196f790678112d928d1a07a70a16ed83
PriteshCK <priteshck.com>
2024-09-11 09:22:57 +0530

    scrapi.py

--- scrapi.py
+++ scrapi.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# Set Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
@@ -14,13 +13,10 @@ chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--force-device-scale-factor=0.1")
  
 
-# query_places.py functionality
 def scrape_places(query):
    
-    # Initialize WebDriver with options
     driver = webdriver.Chrome(options=chrome_options)
     
-    # Determine the search URL
     if "https://www.google.com/maps/place" in query:
         print("coming soon")
         return []
@@ -28,9 +24,7 @@ def scrape_places(query):
         search_url = "https://www.google.com/maps/search/" + query + "+near+me"
     
     driver.get(search_url)
-    driver.implicitly_wait(10)  # Wait for the page to load
     
-    # Parse page source
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     target_class = "Nv2PK THOPZb CpccDe"
     elements = soup.find_all(class_=target_class)
@@ -52,13 +46,10 @@ def save_places_to_file(urls):
         for url in urls:
             file.write(url + '\n')
 
-# get_data.py functionality
 def scrape_data_from_url(url):
    
-    # Initialize WebDriver
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-    driver.implicitly_wait(10)  # Wait for the page to load
     
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     target_class = "rogA2c"
@@ -87,7 +78,6 @@ def scrape_all_places():
     for url in urls:
         scrape_data_from_url(url)
 
-# jjson.py functionality
 def extract_info_from_file():
     with open('details.txt', 'r') as file:
         content = file.read()
@@ -122,24 +112,18 @@ def extract_info_from_file():
             'contact_number': contact_number
         })
     
-    # Write extracted data to JSON file
     with open('data.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
     
     print("Data has been written to data.json.")
 
-# Main program to orchestrate all the steps
 def main():
-    # Step 1: Query places
     query = input("url/query: ")
     urls = scrape_places(query)
     save_places_to_file(urls)
     
-    # Step 2: Scrape data from places
-    open('details.txt', 'w').close()  # Clear the file
     scrape_all_places()
     
-    # Step 3: Extract information and save to JSON
     extract_info_from_file()
 
 if __name__ == "__main__":
