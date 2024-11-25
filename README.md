abd8d3d3eef4946101cf3ceeb143be8fb503dac4
PriteshCK <priteshck.com>
2024-11-25 11:01:30 +0530

    update Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gmaps-scrapi
+
+## How to run:
+1. run the flask service `python service.py`
+2. fetch results using 
+`curl -X POST http://127.0.0.1:5000/scrape -H "Content-Type: application/json" -d '{"query": "hotels+kochi"}'`
+3. queries <= 15 would be in data.json
--- service.py
+++ service.py
@@ -8,10 +8,10 @@ import os
 
 # Set Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--window-size=1920,1080")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--force-device-scale-factor=0.1")
 
 # Initialize Flask app
